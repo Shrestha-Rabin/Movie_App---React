1e30f26d605bf609053b6439e1d3db7826c07459
Rabin Shrestha <rabin3shrestha@gmail.com>
2025-04-02 22:30:45 +0545

    search function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 import Search from './components/Search'
+import Spinner from './components/Spinner'
+import MovieCard from './components/MovieCard';
+
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 function App() {
 
-  const [searchTerm, setSearchterm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [movieList, setMovieList] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  
+
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try { 
+      const endpoint = query 
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS); 
+
+      if(!response.ok) {
+        throw new Error("Failed to fetch movies.");
+      }
+
+      const data = await response.json();
+      
+      if(data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+      
+    } catch (error) {
+        console.error(`Error fetching movies ${error}`);
+        setErrorMessage("Error fetching movies. Please try again later.")
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(searchTerm);
+  }, [searchTerm])
 
   return (
     <>
@@ -15,11 +71,29 @@ function App() {
 
         <div className='wrapper'>
           <header>
-          <img src="./hero.png" alt="Hero banner" />
-          <h1><span className='text-gradient'>Movies</span> For You</h1>
+            <img src="./hero.png" alt="Hero banner" />
+            <h1><span className='text-gradient'>Movies</span> For You</h1>
+            <Search searchTerm={searchTerm} setSearchterm={setSearchTerm} />
           </header>
 
-          <Search searchTerm={searchTerm} setSearchterm={setSearchterm} />
+          <section className='all-movies'>
+            <h2 className='mt-[40px]'>All Movies</h2>
+
+            {isLoading ? (
+              <Spinner />
+            ) : errorMessage ? (
+              <p className='text-red-500'>{errorMessage}</p>
+            ) : (
+              <ul>
+                  {movieList.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                    // <p key={movie.id} className='text-white'>{movie.title}</p>
+                  ))}
+              </ul>
+            )}
+
+          </section>
+
         </div>
       </main>
     </>
